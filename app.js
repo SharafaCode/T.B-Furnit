98c24f1bde7710742e14c522577e6ea1ec3dfa13
sharafa <sharafaoluwatobi@gmail.com>
2022-06-24 00:39:10 +0100

    T.B Furni

---
+++ app.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector(' nav .nav-list .hamburger ');
+const mobile_menu = document.querySelector(' nav .nav-list .nav-bar ul ');
+const header = document.querySelector(' header');
+
+
+hamburger.addEventListener('click', () =>{
+    hamburger.classList.toggle('active');
+   mobile_menu.classList.toggle('active');
+});
+
+
+document.addEventListener('scroll', () =>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 200){
+        header.style.backgroundColor = '#1b1c26';
+    }
+    else{
+        header.style.backgroundColor = 'transparent';
+    }
+
+});
